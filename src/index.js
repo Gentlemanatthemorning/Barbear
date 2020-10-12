4118386288c034fe000052085bf6828347345c2a
Sergey Kalchenko <sergei.alex.kalchenko@gmail.com>
2020-10-12 19:46:27 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-import './scss/main.scss';
-
+import "./scss/main.scss";
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.page_nav_list');
-    const navLinks = document.querySelectorAll('.page_nav_list li');
-
-    burger.addEventListener('click', () => {
-        //Toggle
-        nav.classList.toggle('page_nav_active');
-
-        //Animation
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".page_nav_list");
+  const navLinks = document.querySelectorAll(".page_nav_list li");
+  burger.addEventListener("click", () => {
+    //Toggle
+    nav.classList.toggle("page_nav_active");
+    //Animation
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
     });
-}
-
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
 navSlide();
